b79c53e0c6ba3cd4d273d33aca14a55c2d240b31
Atanas Palavrov <palavrov@gmail.com>
2016-05-10 20:47:33 +0200

    Beautify test output.

--- test/test.js
+++ test/test.js
@@ -1,15 +1,14 @@
 var digestUtils = require('digest-auth-utils');
 
 var serverReply = 'WWW-Authenticate: Digest \
-                          realm="testrealm@host.com",\
-                          qop="auth,auth-int",\
-                          nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093",\
-                          opaque="5ccc069c403ebaf9f0171e9517f40e41"';
-console.log(serverReply);
+ realm="testrealm@host.com",\
+ qop="auth,auth-int",\
+ nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093",\
+ opaque="5ccc069c403ebaf9f0171e9517f40e41"';
 
 var challenge = digestUtils.parseServerChallenge(serverReply);
-console.log(challenge);
+console.log("Chalenge: "+serverReply);
 
 var authHeader = digestUtils.generateRequestHeader(1, challenge, "Mufasa", "Circle Of Life", "GET", "/dir/index.html");
-console.log(authHeader);
+console.log("Authorization: "+authHeader);
 
